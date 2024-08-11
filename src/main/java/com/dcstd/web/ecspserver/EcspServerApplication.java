941ce2ad799ea11e32e4d67aa2b0fb20042ecbb6
Nanoic <2026256242@qq.com>
2024-08-12 00:25:04 +0800

    base

---
+++ src/main/java/com/dcstd/web/ecspserver/EcspServerApplication.java
@@ -0,0 +1,14 @@
+package com.dcstd.web.ecspserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*  */
+@SpringBootApplication
+public class EcspServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcspServerApplication.class, args);
+    }
+
+}
