e20faea181cbe74d8acfe39bba2c7b189710db04
Nanoic <2026256242@qq.com>
2024-08-16 18:59:21 +0800

    添加注释
    【新增】
    全局配置信息添加说明注释

--- src/main/java/com/dcstd/web/ecspserver/config/GlobalConfiguration.java
+++ src/main/java/com/dcstd/web/ecspserver/config/GlobalConfiguration.java
@@ -10,35 +10,37 @@ public class GlobalConfiguration {
 
     /* 版本信息 */
     @Value("${custom-config.app-info.app-name}")
-    private String appName;
+    private String appName;//应用名称
     @Value("${custom-config.app-info.app-version}")
-    private String appVersion;
+    private String appVersion;//版本号
 
     /* 小程序信息 */
     @Value("${custom-config.app-info.appid}")
-    private String appid;
+    private String appid;//小程序appid
     @Value("${custom-config.app-info.secret}")
-    private String secret;
+    private String secret;//小程序secret
     @Value("${custom-config.app-info.grant-type}")
-    private String grantType;
+    private String grantType;//授权类型
 
     /* URL信息 */
     @Value("${custom-config.url.wx-login}")
-    private String wxLogin;
+    private String wxLogin;//微信登录接口登录地址
     @Value("${custom-config.url.jw}")
-    private String jw;
+    private String jw;//教务系统地址
     @Value("${custom-config.url.jw-get-public-key}")
-    private String jwGetPublicKey;
+    private String jwGetPublicKey;//获取公钥地址
     @Value("${custom-config.url.jw-cookie}")
-    private String jwCookie;
+    private String jwCookie;//获取cookie地址
     @Value("${custom-config.url.jw-kaptcha}")
-    private String jwKaptcha;
+    private String jwKaptcha;//验证码地址
     @Value("${custom-config.url.jw-login}")
-    private String jwLogin;
+    private String jwLogin;//教务系统登录地址
 
     /* 时间信息 */
     @Value("${custom-config.time.token-expire-time}")
-    private int tokenExpireTime;
+    private int tokenExpireTime;//token超时时间(秒)
+    @Value("${custom-config.time.cors-expire-time}")
+    private int corsExpireTime;//当前跨域请求最大有效时长(秒)
 
 
 }
